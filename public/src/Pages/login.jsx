37b6ae53fed9eb7f7ae1524200d4f4fe10de6347
Administrator <root@jsprogrammer.net>
2021-10-08 18:00:55 +0530

    login ui design

--- public/src/Pages/login.jsx
+++ public/src/Pages/login.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Button, Card, CardContent, TextField, Typography } from '@material-ui/core'
+import GoogleButton from 'react-google-button';
 
 export default class login extends Component {
     constructor(props) {
@@ -11,12 +13,33 @@ export default class login extends Component {
     render() {
         return (
             <div className="Login" >
-                <div className="Box">
-                    <div className="Login_Hader">
-                        Login
-                    </div>
-                    <div className="Login_Body"></div>
-                </div>
+                <Card style={{ width:'90%', height:'90%', maxHeight:'350px', maxWidth:"300px", }} >
+                    <CardContent>
+                        <Typography component='div' >
+                        <div className="LoginHader">
+                            Login
+                        </div>
+                        </Typography>
+                        <div style={{ display:"flex", justifyContent:'center', alignItems:'center', padding:'10px' }}>
+                            <TextField id="phoneNo" label="Mobile no" variant="outlined" style={{ width:'240px' }} />
+                        </div>
+                        <div style={{ display:"flex", justifyContent:'center', alignItems:'center', padding:'10px'}}>
+                            <TextField id="phoneNo" label="Password" variant="outlined" style={{ width:'240px' }} />
+                        </div>
+                        <div style={{ display:"flex", justifyContent:'center', alignItems:'center', padding:'10px'}}>
+                            <Button variant="contained" color="primary" style={{ width:'240px' }} onClick={()=>{
+                                console.log('Clicked By Mobile No.');
+                            }} >Login</Button  >
+                        </div>
+                        <div style={{ display:"flex", justifyContent:'center', alignItems:'center', padding:'10px'}}>
+                            <GoogleButton
+                                onClick={()=>{
+                                    console.log(`Clicked By Gmail Account`);
+                                }}
+                            />
+                        </div>
+                    </CardContent>
+                </Card>
             </div>
         )
     }
